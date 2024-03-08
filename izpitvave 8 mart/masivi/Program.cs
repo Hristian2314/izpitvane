68e68d08a525e146c905b9a8255cbda29ca05fd7
Hristian2314 <f75@abv.bg>
2024-03-08 13:44:00 +0200

    first commit

---
+++ izpitvave 8 mart/masivi/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+
+namespace masivi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+               
+            }
+
+            Console.Write(string.Join(" ,", array));
+
+        }
+    }
+}
